e8cc46c67547427938e5a4c15106fcf17e656008
erikyoung <erik.young@rmit.edu.vn>
2017-09-05 00:16:30 +0700

    added Note and List components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
 class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
   render() {
+    const { showNote } = this.state;
+
+
     return (
       <div className="App">
-      <div>
         <Nav />
-      </div>
+        { showNote ? <Note /> : <List /> }
       </div>
     );
   }

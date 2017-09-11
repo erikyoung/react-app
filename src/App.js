60f6a723689d8a7bcc386d0d78930ce142fa0735
erikyoung <erik.young@rmit.edu.vn>
2017-09-11 22:45:59 +0700

    finalized the note app for react.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
 import urlFor from './helpers/urlFor';
+import Flash from './components/Flash';
 
 class App extends Component {
   constructor () {
@@ -14,7 +15,8 @@ class App extends Component {
       showNote: false,
       notes: [], 
       note: {}, 
-      newTag: false
+      newTag: false, 
+      error: ''
     };
   }
 
@@ -50,8 +52,17 @@ class App extends Component {
   submitNote = (data, id) => {
     this.performSubmissionRequest(data, id)
     .then((res) => this.setState({ showNote: false }) )
-    .catch((err) => console.log(err.response.data));
-  }
+    .catch((err) => {
+      const { errors } = err.response.data;
+      if (errors.content) {
+        this.setState({ error: "Missing Note Content!"});
+      } else if (errors.title) {
+        this.setState({ error: "Missing Note Title"});
+      }
+    
+  });
+
+}
 
   deleteNote = (id) => {
     const newNotesState = this.state.notes.filter((note) => note.id !== id);
@@ -71,8 +82,13 @@ class App extends Component {
   submitTag = (data, noteId) => {
     axios.post(urlFor(`notes/${noteId}/tags`), data)
     .then((res) => this.getNote(noteId) )
-  .catch((err) => console.log(err.response.data) );
-  }
+  .catch((err) => {
+    const { errors } = err.response.data;
+    if (errors.name) {
+      this.setState({ error: "Missing Tag Name!"});
+    }
+  });
+}
 
   deleteTag = (noteId, id) => {
     axios.delete(urlFor(`/tags/${id}`))
@@ -80,13 +96,24 @@ class App extends Component {
   .catch((err) => console.log(err.response.data) );
   }
 
+  deleteTag = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
+  resetError = () => {
+    this.setState({ error: ''});
+  }
+
   render() {
-    const { showNote, notes, note, newTag } = this.state;
+    const { showNote, notes, note, newTag, error } = this.state;
 
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
+       {error && <Flash error = {error} resetError={this.resetError} />}
         { showNote ? 
           <Note 
             note={note}
